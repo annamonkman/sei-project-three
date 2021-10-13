20c1d9c4ade57c28b2615511172086b55d0893cd
Anna Monkman <annamonkman@me.com>
2021-10-13 17:25:28 +0100

    starting again after deploy to heroku GA advice not working - app working well

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,9 +1,20 @@
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/sei-project3'
-const port = process.env.PORT || 4000
-const secret = process.env.SECRET || 'Alright then, keep your secrets'
 
-module.exports = {
-  dbURI,
-  port,
-  secret
-}
+export const port = 4000
+export const dbURI = 'mongodb://localhost/sei-project-three'
+export const secret = 'Alright then, keep your secrets'
+
+
+// ? GA HEROKU DEPLOY ADVICE CODE
+
+// export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/sei-project-three'
+// export const port = process.env.PORT || 4000
+// export const secret = process.env.SECRET || 'Alright then, keep your secrets'
+
+// module.exports = {
+//   dbURI,
+//   port,
+//   secret
+// }
+
+
+
--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,66 @@
-const express = require('express')
-const mongoose = require('mongoose')
+import express from 'express'
+import mongoose from 'mongoose'
+import { port, dbURI } from './config/environment.js'
+import router from './config/router.js'
+
 const app = express()
-const logger = require('./lib/logger')
-const router = require('./config/routes')
-const errorHandler = require('./lib/errorHandler')
-const { dbURI, port } = require('./config/environment')
 
-mongoose.connect(
-  dbURI,
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true  },
-  (err) => {
-    if (err) return console.log(err)
-    console.log('Mongo is Connected!')
-  })
+const startServer = async () => {
+  try {
+    await mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    console.log('🚀 Database has connected successfully')
 
-app.use(express.static(`${sei-project-three}/frontend/build`)) // <-- This line has been added before the express json middleware, it will allow the app to respond to a request with contents of this directory "build", which will contain our React App code.
+    app.use(express.json())
 
-app.use(express.json())
+    app.use((req, _res, next) => {
+      console.log(`🚨 Incoming request: ${req.method} - ${req.url}`)
+      next()
+    })
 
-app.use(logger)
+    app.use('/api', router)
 
-app.use('/api', router)
+    app.listen(port, () => console.log(`🚀 Express is up and running on port ${port}`))
+  } catch (err) {
+    console.log('🆘 Something went wrong starting the app')
+    console.log(err)
+  }
+}
+startServer()
 
-app.use('/*', (_, res) => res.sendFile(`${sei-project-three}/frontend/build/index.html`)) // <-- This additional route handler has been added between the router and error handler middleware it means that any incoming request that does not match a route in router should respond back with our frontend.
 
-app.use(errorHandler)
 
-app.listen(port, () => console.log(`Express is listening on port ${port}`))
+// ? FOR HEROKU DEPLOYMENT GA ADVICE RUNNING FRONTEND ON BACKEND
+
+// import express from 'express'
+// import mongoose from 'mongoose'
+// const app = express()
+// import logger from './lib/logger'
+// import router from './config/router.js'
+// import errorHandler from './lib/errorHandler'
+// import { dbURI, port } from './config/environment.js'
+
+// import path from 'path'
+
+// const __dirname = path.resolve()
+
+// mongoose.connect(
+//   dbURI,
+//   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true  },
+//   (err) => {
+//     if (err) return console.log(err)
+//     console.log('Mongo is Connected!')
+//   })
+
+// app.use(express.static(`${__dirname}/client/build`)) // <-- This line has been added before the express json middleware, it will allow the app to respond to a request with contents of this directory "build", which will contain our React App code.
+
+// app.use(express.json())
+
+// app.use(logger)
+
+// app.use('/api', router)
+
+// app.use('/*', (_, res) => res.sendFile(`${__dirname}/client/build/index.html`)) // <-- This additional route handler has been added between the router and error handler middleware it means that any incoming request that does not match a route in router should respond back with our frontend.
+
+// app.use(errorHandler)
+
+// app.listen(port, () => console.log(`Express is listening on port ${port}`))
